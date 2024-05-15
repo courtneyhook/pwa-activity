20a7451d2cef0a5e51a923616541ef8af09beed3
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-05-15 16:08:19 -0500

    everything works except get and put

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = () => {
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
+      editor: "./src/js/editor.js",
     },
     output: {
       filename: "[name].bundle.js",
@@ -46,8 +47,8 @@ module.exports = () => {
         display: "standalone",
         background_color: "#225ca3",
         theme_color: "#225ca3",
-        start_url: "./",
-        publicPath: "./",
+        start_url: "/",
+        publicPath: "/",
       }),
     ],
 
@@ -59,11 +60,11 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules | bower.component)/,
+          exclude: /node_modules/,
           use: {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset.env"],
+              presets: ["@babel/preset-env"],
               plugins: [
                 "@babel/plugin-proposal-object-rest-spread",
                 "@babel/transform-runtime",
